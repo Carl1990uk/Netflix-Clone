b8e4e1cd47071668783909c379a47fd838bebca3
Carl Clarke <carlclarke1990@me.com>
2022-09-27 16:04:52 +0200

    handleclick function for left & right

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -2,7 +2,8 @@ import Image from "next/image"
 import { Movie } from "../typings"
 
 interface Props{
-    movie: Movie
+    movie: Movie 
+    // movie: Movie | DocumentData firebase
 }
 
 function Thumbnail({movie}: Props) {
